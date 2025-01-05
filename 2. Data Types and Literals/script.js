c205d68d2937eca77612dbbc91444a68324ca17e
ADITYA ADARSH <adityachauhan3498@gmail.com>
2025-01-05 08:24:47 +0530

    Addin few more examples of Data Types

--- 2. Data Types and Literals/script.js
+++ 2. Data Types and Literals/script.js
@@ -1,18 +1,24 @@
-console.log(typeof "Hello");  // Data Type -> String, Literal value -> "Hello"
+console.log(typeof "Hello");  // Data Type -> String, Literal Value -> "Hello"
 
 let num = 80;
 
-console.log(typeof num);   // Data Type -> String, Literal value -> 80
+console.log(typeof num);   // Data Type -> String, Literal Value -> 80
 
-console.log(typeof true);  // Data Type -> Boolean, Literal value -> true
+console.log(typeof true);  // Data Type -> Boolean, Literal Value -> true
 
-console.log(typeof {welcome:"Hello..."});  // Data Type -> Object, Literal value -> {welcome:"Hello..."}
+console.log(typeof 123n);  // Data Type -> Bigint, Literal Value -> 123n
+
+console.log(typeof "A" + "A");  // Data Type -> StringA, Literal Value -> stringA
+
+console.log(typeof "A" * "A");  // Data Type -> NaN, Literal Value -> NaN
+
+console.log(typeof {welcome:"Hello..."});  // Data Type -> Object, Literal Value -> {welcome:"Hello..."}
 
 
-console.log(typeof [1,2,3]);  // Data Types -> Object, Literal value -> [1,2,3]
+console.log(typeof [1,2,3]);  // Data Types -> Object, Literal Value -> [1,2,3]
 
 a = [1,2,3];
-console.log(typeof a);     // Data Types -> Object, Literal value -> [1,2,3], Consider README.md file for more clarification
+console.log(typeof a);     // Data Types -> Object, Literal Value -> [1,2,3], Consider README.md file for more clarification
 
 // To specifically determine if a variable is an array, use the following methods:-
 
@@ -27,7 +33,7 @@ function great() {
 }
 
 console.log(typeof great);         // Data Type -> function
-// The literal value of the function great() is the function itself, defined using the function keyword, along with its body and behavior.
+// The literal Value of the function great() is the function itself, defined using the function keyword, along with its body and behavior.
 
 // We Can declare a function in this type also, call in the same way as above
 
