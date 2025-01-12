c503e1c397757fbd11649518cd528cf58dc8e6c5
ADITYA ADARSH <adityachauhan3498@gmail.com>
2025-01-12 12:54:23 +0530

    Hosting folder added

--- 8. Hosting/script.js
+++ 8. Hosting/script.js
@@ -1,6 +1,13 @@
+console.log("**************************");
+console.log("Hoisting in Variables");
+
 console.log(x);                // Output: undefined 
 var x = 10; 
 
+
+
+console.log("**************************");
+console.log("Hoisting in Functions->Function Declarations");
 
 myFunction();                  // Output: Hello from myFunction
 function myFunction() {
@@ -8,17 +15,27 @@ function myFunction() {
 }
 
 
+
+console.log("**************************");
+console.log("Hoisting in Functions->Function Expressions");
+
 greet();                       // TypeError: greet is not a function
 var greet = function () {
   console.log("Nice to meet you!");
 };
 
 
+console.log("**************************");
+console.log("Hoisting in Function");
+
 console.log(myFunction);       // Output: undefined
 var myFunction = function() {
   console.log("Hello from myFunction");
 };
 
+
+console.log("**************************");
+console.log("Hoisting in Classes");
 
 const obj = new MyClass();     // ReferenceError
 class MyClass {
