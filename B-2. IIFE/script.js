33be8b464144e5c3d4b812c9edc18033634828b9
ADITYA ADARSH <adityachauhan3498@gmail.com>
2025-01-18 09:00:09 +0530

    Using IIFE with Arrow function.

--- B-2. IIFE/script.js
+++ B-2. IIFE/script.js
@@ -11,3 +11,10 @@ chai();
     
 })();                  // IIFE's must be ended with semicolon ;
                        // IIFEs need a semicolon to clearly tell the JavaScript engine where the IIFE ends, preventing confusion and potential errors.
+
+// Arrow Function IIFE:
+
+(()=>{
+    console.log("Have a cup of tea.");
+    
+})();
