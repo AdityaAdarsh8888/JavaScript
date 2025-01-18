dcdcf2b2d60cdaf5ff404ac908c0328ae8d47cb7
ADITYA ADARSH <adityachauhan3498@gmail.com>
2025-01-18 09:15:40 +0530

    Some more content is added in IIFE

--- B-2. IIFE/script.js
+++ B-2. IIFE/script.js
@@ -12,9 +12,18 @@ chai();
 })();                  // IIFE's must be ended with semicolon ;
                        // IIFEs need a semicolon to clearly tell the JavaScript engine where the IIFE ends, preventing confusion and potential errors.
 
-// Arrow Function IIFE:
+
+/*Arrow Function IIFE*/
 
 (()=>{
     console.log("Have a cup of tea.");
     
 })();
+
+
+/*IIFE with Parameters*/
+
+((name)=>{
+    console.log(`${name},please have a cup of tea.`);
+    
+})(`Aditya Adarsh`);
